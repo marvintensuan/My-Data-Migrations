b88ba59d0856e893f3d78cdba93248c0139207f6
Marvin <mdtensuan_dev@protonmail.com>
2021-02-13 16:34:37 +0800

    Remove column 'id'

--- 2021-02-09 postgresql_to_firestore/helper.py
+++ 2021-02-09 postgresql_to_firestore/helper.py
@@ -6,11 +6,16 @@ def list_of_dictionaries(column_names, table):
     data_list = []
     default_time = time(0,0)
     for row in table:
+        # Convert values to native Python data types.
         row_list = list(row)
         for n, item in enumerate(row_list):
             if isinstance(item, date):
                 row_list[n] = datetime.combine(item, default_time)
             elif isinstance(item, Decimal):
                 row_list[n] = float(item)
-        data_list.append(dict(zip(column_names, row_list)))
+        
+        # Remove 'id' column
+        row_dict = dict(zip(column_names, row_list))
+        row_dict.pop('id')
+        data_list.append(row_dict)
     return data_list
